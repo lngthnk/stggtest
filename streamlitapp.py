ee6fc745fb71e02c92582c544ef3bde75e3b6cec
spdgbs88 <spdgbs88@gmail.com>
2024-08-26 15:59:01 +0700

    fix sheet name

--- streamlitapp.py
+++ streamlitapp.py
@@ -4,12 +4,12 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object for the first spreadsheet
-conn_one = st.connection("name_one", type=GSheetsConnection)
+conn_one = st.connection("gsheets1", type=GSheetsConnection)
 df_one = conn_one.read()
 
 
 # Create a connection object for the second spreadsheet
-conn_two = st.connection("name_two", type=GSheetsConnection)
+conn_two = st.connection("gsheets2", type=GSheetsConnection)
 df_two = conn_two.read()
 st.header('df1')
 st.dataframe(df_one)
